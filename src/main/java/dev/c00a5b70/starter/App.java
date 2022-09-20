ecd7b9a8880fcbc69df537ad0391cf2bfe0b1159
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-20 15:05:27 -0600

    Initial Checkin

---
+++ src/main/java/dev/c00a5b70/starter/App.java
@@ -0,0 +1,13 @@
+package dev.c00a5b70.starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
