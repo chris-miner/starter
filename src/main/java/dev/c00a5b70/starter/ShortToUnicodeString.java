aa78d8b34625c20f0bc6451699c05f8872320483
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-21 16:30:15 -0600

    created new translate app using base listener extension and launch config.

---
+++ src/main/java/dev/c00a5b70/starter/ShortToUnicodeString.java
@@ -0,0 +1,19 @@
+package dev.c00a5b70.starter;
+
+public class ShortToUnicodeString extends ArrayInitBaseListener {
+    @Override
+    public void enterInit(ArrayInitParser.InitContext ctx) {
+        System.out.print('"');
+    }
+
+    @Override
+    public void exitInit(ArrayInitParser.InitContext ctx) {
+        System.out.print('"');
+    }
+
+    @Override
+    public void enterValue(ArrayInitParser.ValueContext ctx) {
+        int value = Integer.valueOf(ctx.INT().getText());
+        System.out.printf("\\u%04x", value);
+    }
+}
