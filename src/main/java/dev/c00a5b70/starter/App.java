16e31eeca5c1139063e4e9c10cda4e8bc3481db6
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-21 16:14:09 -0600

    FIFY reconfigured project to work as it should. by moving the grammar file to the "right" place.  Generated source is both found and used.  Joy.

--- src/main/java/dev/c00a5b70/starter/App.java
+++ src/main/java/dev/c00a5b70/starter/App.java
@@ -1,13 +1,23 @@
 package dev.c00a5b70.starter;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        CharStream input = CharStreams.fromStream(System.in);
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        ParseTree tree = parser.init();
+        System.out.println(tree.toStringTree(parser));
     }
 }
